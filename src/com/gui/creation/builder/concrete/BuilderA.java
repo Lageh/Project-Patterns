78d5165879cd3aec4ce509877d922b2410ab5fed
Guilherme Guerra <guilherme0895@gmail.com>
2023-06-30 02:34:50 -0300

    Builder Method

---
+++ src/com/gui/creation/builder/concrete/BuilderA.java
@@ -0,0 +1,32 @@
+package com.gui.creation.builder.concrete;
+
+import com.gui.creation.builder.contracts.Builder;
+import com.gui.creation.builder.products.Product;
+import com.gui.creation.builder.products.ProductA;
+
+public class BuilderA implements Builder {
+    private ProductA productA;
+
+    @Override
+    public void reset() {
+        this.productA = new ProductA();
+    }
+
+    @Override
+    public void setName(String name) {
+        productA.setName(name);
+    }
+
+    @Override
+    public void setQuantity(int quantity) {
+        productA.setQuantity(9);
+    }
+
+    public Product getProduct() {
+        return new ProductA();
+    }
+
+    public BuilderA() {
+        System.out.println("Setting up builder 2");
+    }
+}
---
+++ src/com/gui/creation/builder/products/ProductB.java
@@ -0,0 +1,31 @@
+package com.gui.creation.builder.products;
+
+public class ProductB extends Product{
+    String parameterD;
+    String parameterE;
+    String parameterF;
+
+    public String getParameterD() {
+        return parameterD;
+    }
+
+    public void setParameterD(String parameterD) {
+        this.parameterD = parameterD;
+    }
+
+    public String getParameterE() {
+        return parameterE;
+    }
+
+    public void setParameterE(String parameterE) {
+        this.parameterE = parameterE;
+    }
+
+    public String getParameterF() {
+        return parameterF;
+    }
+
+    public void setParameterF(String parameterF) {
+        this.parameterF = parameterF;
+    }
+}
